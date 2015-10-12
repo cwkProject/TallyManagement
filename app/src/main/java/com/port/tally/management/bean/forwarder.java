1c414543d3cfe5e971f9e0402036dce57d8c4b94
超悟空 <39251474@163.com>
2015-10-12 09:54:33 +0800

    查询系统

---
+++ app/src/main/java/com/port/tally/management/bean/forwarder.java
@@ -0,0 +1,14 @@
+package com.port.tally.management.bean;
+/**
+ * Created by 超悟空 on 2015/10/10.
+ */
+
+/**
+ * 类描述
+ *
+ * @author 超悟空
+ * @version 1.0 2015/10/10
+ * @since 1.0
+ */
+public class forwarder {
+}
