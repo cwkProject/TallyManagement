c594fd06b3e2429c7ca3a207beb33bb3d51b3b77
超悟空 <39251474@163.com>
2015-10-20 12:50:06 +0800

    查询模块添加加载更多机能，主界面重新布局

--- app/src/main/java/com/port/tally/management/activity/MainActivity.java
+++ app/src/main/java/com/port/tally/management/activity/MainActivity.java
@@ -4,31 +4,29 @@ package com.port.tally.management.activity;
  */
 
 import android.content.Intent;
-import android.content.res.TypedArray;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
-import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.port.tally.management.R;
-import com.port.tally.management.adapter.MessgaePushAdapter;
+import com.port.tally.management.adapter.MainFunctionRecyclerViewAdapter;
 import com.port.tally.management.adapter.PushMessageAdapter;
-import com.port.tally.management.adapter.TallyManageAdapter;
+import com.port.tally.management.holder.MainFunctionItemViewHolder;
 import com.port.tally.management.util.StaticValue;
 import com.port.tally.management.work.ToallyManageWork;
 import com.port.tally.management.xlistview.XListView;
 
 import org.mobile.library.common.function.CheckUpdate;
+import org.mobile.library.model.operate.OnItemClickListenerForRecyclerViewItem;
 import org.mobile.library.model.work.WorkBack;
 import org.mobile.library.util.LoginStatus;
 
@@ -43,32 +41,20 @@ import static com.port.tally.management.adapter.FunctionIndex.toFunction;
  * 主界面
  *
  * @author 超悟空
- * @version 1.0 2015/9/7
+ * @version 2.0 2015/10/20
  * @since 1.0
  */
 public class MainActivity extends AppCompatActivity implements XListView.IXListViewListener {
     private TextView moreTv;
     private XListView listView;
-    private ArrayAdapter<String> mAdapter;
     private PushMessageAdapter pushMessageAdapter;
     private List<Map<String, Object>> dataList = null;
-    private Handler mHandler;
     int flag = 1;
     private Toast mToast;
     /**
      * 日志标签前缀
      */
     private static final String LOG_TAG = "MainActivity.";
-
-    /**
-     * 功能名称取值标签
-     */
-    private static final String FUNCTION_NAME = "function_name";
-
-    /**
-     * 功能图标取值图标
-     */
-    private static final String FUNCTION_IMAGE = "function_image";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -132,36 +118,33 @@ public class MainActivity extends AppCompatActivity implements XListView.IXListV
     }
 
     /**
-     * 设置表格适配器，否则使用默认配置
-     *
-     * @return 简单适配器对象
-     */
-    protected SimpleAdapter onGridSimpleAdapter() {
-        return new SimpleAdapter(this, initFunctionResource(), R.layout.function_grid_item, new
-                String[]{FUNCTION_NAME , FUNCTION_IMAGE}, new int[]{R.id.function_grid_item_text
-                , R.id.function_grid_item_image});
-    }
-
-    /**
      * 初始化表格布局
      */
     private void initGridView() {
-        // 片段中的表格布局
-        GridView gridView = (GridView) findViewById(R.id.activity_main_gridView);
 
-        // 表格使用的数据适配器
-        SimpleAdapter adapter = onGridSimpleAdapter();
+        // RecyclerView列表对象
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.activity_main_recyclerView);
 
-        // 设置适配器
-        gridView.setAdapter(adapter);
+        // 创建布局管理器
+        GridLayoutManager gridlayoutManager = new GridLayoutManager(this, 2);
 
-        // 设置监听器
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        // 设置布局管理器
+        recyclerView.setLayoutManager(gridlayoutManager);
+
+        // 新建数据适配器
+        MainFunctionRecyclerViewAdapter adapter = new MainFunctionRecyclerViewAdapter(this);
+
+        // 设置点击事件
+        adapter.setOnItemClickListener(new OnItemClickListenerForRecyclerViewItem<String[],
+                MainFunctionItemViewHolder>() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                onGridItemClick(view, position);
+            public void onClick(String[] dataSource, MainFunctionItemViewHolder holder) {
+                onGridItemClick(holder.getAdapterPosition());
             }
         });
+
+        // 绑定数据适配器
+        recyclerView.setAdapter(adapter);
     }
 
     /**
@@ -169,10 +152,9 @@ public class MainActivity extends AppCompatActivity implements XListView.IXListV
      * 默认触发功能跳转，
      * 并检测登录状态
      *
-     * @param view     当前点击的功能布局对象
      * @param position 点击的位置索引
      */
-    protected void onGridItemClick(View view, int position) {
+    private void onGridItemClick(int position) {
 
         if (!LoginStatus.getLoginStatus().isLogin()) {
             // 未登录
@@ -186,41 +168,6 @@ public class MainActivity extends AppCompatActivity implements XListView.IXListV
 
         // 跳转到功能
         toFunction(this, position);
-    }
-
-    /**
-     * 生成功能项标签资源的数据源
-     *
-     * @return 返回SimpleAdapter适配器使用的数据源
-     */
-    private List<Map<String, Object>> initFunctionResource() {
-        // 加载功能项
-        // 资源集合
-        List<Map<String, Object>> dataList = new ArrayList<Map<String, Object>>();
-
-        // 功能名称数组
-        String[] functionTitle = getResources().getStringArray(R.array
-                .grid_item_function_name_list);
-        // 资源类型数组
-        TypedArray images = getResources().obtainTypedArray(R.array.grid_item_function_image_list);
-
-        for (int i = 0; i < functionTitle.length; i++) {
-
-            // 新建一个功能项标签
-            Map<String, Object> function = new HashMap<>();
-
-            // 添加功能标签名称资源
-            function.put(FUNCTION_NAME, functionTitle[i]);
-
-
-            // 添加功能标签图标资源
-            function.put(FUNCTION_IMAGE, images.getResourceId(i, R.mipmap.ic_launcher));
-
-            // 将标签加入数据源
-            dataList.add(function);
-        }
-
-        return dataList;
     }
 
     @Override
@@ -251,7 +198,7 @@ public class MainActivity extends AppCompatActivity implements XListView.IXListV
     }
 
     //显示数据
-    private void showData(String key, String type, String company,String cargo,String trustno) {
+    private void showData(String key, String type, String company, String cargo, String trustno) {
         key = "3";
         type = "1";
         company = "14";
@@ -263,8 +210,8 @@ public class MainActivity extends AppCompatActivity implements XListView.IXListV
         String count = "5";
         String stratcount = String.valueOf(flag);
         String company = "14";
-        String cargo=null;
-        String trustno =null;
+        String cargo = null;
+        String trustno = null;
         loadValue(count, stratcount, company, cargo, trustno);
     }
 
@@ -276,11 +223,12 @@ public class MainActivity extends AppCompatActivity implements XListView.IXListV
     }
 
     public void onRefresh() {
-//        showData();
+        //        showData();
     }
 
     //给个控件赋值
-    private void loadValue(String key, final String type, String company,String cargo,String trustno) {
+    private void loadValue(String key, final String type, String company, String cargo, String
+            trustno) {
 
         //实例化，传入参数
         ToallyManageWork toallyManageWork = new ToallyManageWork();
@@ -303,12 +251,11 @@ public class MainActivity extends AppCompatActivity implements XListView.IXListV
                     pushMessageAdapter.notifyDataSetChanged();
                 } else {
                     //清空操作
-//                    listView.setPullLoadEnable(false);
-//                    for(int i =0;i<1;i++){
+                    //                    listView.setPullLoadEnable(false);
+                    //                    for(int i =0;i<1;i++){
                     showToast("无相关信息");
-//                        tallyManageAdapter.notifyDataSetChanged();
-//                    }
-
+                    //                        tallyManageAdapter.notifyDataSetChanged();
+                    //                    }
 
 
                 }
@@ -337,23 +284,20 @@ public class MainActivity extends AppCompatActivity implements XListView.IXListV
 
             public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
 
-//                String cgno = "14";
-//                Bundle b = new Bundle();
-//                Intent intent = new Intent();
-//                //                b.putStringArray("Cgno", cgno);
-//                b.putString("Cgno", cgno);
-//                intent = new Intent(TallyActivity.this, TallyDetail.class);
-//                intent.putExtras(b);
-//                startActivity(intent);
+                //                String cgno = "14";
+                //                Bundle b = new Bundle();
+                //                Intent intent = new Intent();
+                //                //                b.putStringArray("Cgno", cgno);
+                //                b.putString("Cgno", cgno);
+                //                intent = new Intent(TallyActivity.this, TallyDetail.class);
+                //                intent.putExtras(b);
+                //                startActivity(intent);
                 HashMap map = (HashMap) arg0.getItemAtPosition(arg2);
-//                派工编码
-//                委托编码
-//                票货编码
-                String[] strings = new String[]{
-                        map.get("pmno").toString(),
-                        map.get("tv_Entrust").toString(),
-                        map.get("gbno").toString()
-                };
+                //                派工编码
+                //                委托编码
+                //                票货编码
+                String[] strings = new String[]{map.get("pmno").toString() , map.get
+                        ("tv_Entrust").toString() , map.get("gbno").toString()};
                 Bundle b = new Bundle();
                 Intent intent = new Intent();
                 b.putStringArray("detailString", strings);
@@ -368,6 +312,7 @@ public class MainActivity extends AppCompatActivity implements XListView.IXListV
 
         });
     }
+
     private void showToast(String msg) {
         if (mToast == null) {
             mToast = Toast.makeText(this, msg, Toast.LENGTH_SHORT);
